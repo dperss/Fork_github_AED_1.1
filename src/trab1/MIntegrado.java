bf1194d46c5f3a4bdb846867d652f7423c290ee1
Joao Paulo Moreira Coelho <a39961@alunos.ipb.pt>
2019-04-09 18:35:53 +0100

    Upload New File

---
+++ src/trab1/MIntegrado.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trab1;
+
+/**
+ *
+ * @author João Coelho nº39961 EI - Diogo Ramos nº39954 EI
+ */
+public class MIntegrado extends Membro{
+    private double saldo;
+    
+    public MIntegrado(int i,String n){
+        super(i,n);
+    }
+    public boolean adicionarAoSaldo(double val){
+        return false; // TEMPORARIO
+    }
+    public String toString(){
+        return "Id do MIntegrado:" + id + "\n Nome:" + super.toString()+"";
+    }
+}
