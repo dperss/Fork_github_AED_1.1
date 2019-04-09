0bbae7c594be19b57f2af657e2afb13008b31780
Joao Paulo Moreira Coelho <a39961@alunos.ipb.pt>
2019-04-09 18:35:44 +0100

    Upload New File

---
+++ src/trab1/Membro.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trab1;
+
+import java.util.LinkedHashSet;
+
+/**
+ *
+ * @author João Coelho nº39961 EI - Diogo Ramos nº39954 EI
+ */
+public abstract class Membro {
+    protected int id;
+    private final String nome;
+    private LinkedHashSet<Projeto> projetos;
+    
+    public Membro(int i,String n){
+        id=i;
+        nome=n;
+        projetos=new LinkedHashSet<>();
+    }
+    public boolean associarProjeto(Projeto p){
+        return projetos.add(p);
+    }
+    public boolean addicionarAoSaldo(double val){ // ABSTRACT
+        return false; //TEMPORARIO
+    }
+    public String toString(){
+        return nome;
+    }
+}
