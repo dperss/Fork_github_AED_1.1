d48f9898d63d2d71c88dd87e5a37dae33c7a0613
Joao Paulo Moreira Coelho <a39961@alunos.ipb.pt>
2019-04-09 18:35:24 +0100

    Upload New File

---
+++ src/trab1/Colaborador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trab1;
+
+/**
+ *
+ * @author João Coelho nº39961 EI - Diogo Ramos nº39954 EI
+ */
+public class Colaborador extends Membro{
+    public Colaborador(int i,String n){
+        super(i,n);
+    }
+    public boolean adicionarAoSaldo(double val){
+        return false; //TEMPORARIO
+    }
+    public String toString(){
+        return "Id do Colaborador:" + id + "\n Nome:" + super.toString()+"";
+    }
+}
