6fb59f8407cd3e80531c089d6a517a1d37469b81
Diogo <dperss@gmail.com>
2019-04-09 17:21:59 +0100

    1-Commit

--- src/trab1/Membro.java
+++ src/trab1/Membro.java
@@ -5,25 +5,31 @@
  */
 package trab1;
 
+import java.util.LinkedHashSet;
+
 /**
  *
  * @author João Coelho nº39961 EI - Diogo Ramos n~39954 EI
  */
 public class Membro {
     protected int id;
-    private String nome;
+    private final String nome;
+    private LinkedHashSet<Projeto> projetos; 
     
     public Membro(int i,String n){
         id=i;
         nome=n;
+        projetos=new LinkedHashSet<>();        
     }
     public boolean associarProjeto(Projeto p){
-        
+        return projetos.add(p);
     }
     public boolean addicionarAoSaldo(double val){
         
+        
+        return false;//temporario
     }
     public String toString(){
-        
+        return nome;
     }
 }
