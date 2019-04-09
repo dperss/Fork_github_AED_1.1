fb27c9bdae93e3d421f6ec933917e7ac767eb60a
Joao Paulo Moreira Coelho <a39961@alunos.ipb.pt>
2019-04-09 18:54:19 +0100

    Update Colaborador.java

--- src/trab1/Colaborador.java
+++ src/trab1/Colaborador.java
@@ -9,14 +9,18 @@ package trab1;
  *
  * @author João Coelho nº39961 EI - Diogo Ramos nº39954 EI
  */
-public class Colaborador extends Membro{
-    public Colaborador(int i,String n){
+public class MIntegrado extends Membro{
+    private double saldo;
+    
+    public MIntegrado(int i,String n){
         super(i,n);
     }
     public boolean adicionarAoSaldo(double val){
-        return false; //TEMPORARIO
+        return true; // TEMPORARIO
     }
+    
+    @Override
     public String toString(){
-        return "Id do Colaborador:" + id + "\n Nome:" + super.toString()+"";
+        return "Id do MIntegrado:" + id + "\n Nome:" + super.toString()+"";
     }
 }
